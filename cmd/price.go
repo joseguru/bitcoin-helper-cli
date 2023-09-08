2ccec20c33178c40a0d95b949d205d5d289c8738
Joseph Murgor <joskip5@gmail.com>
2023-09-08 09:20:53 +0300

    command descripption

--- cmd/price.go
+++ cmd/price.go
@@ -9,16 +9,16 @@ import (
 
 	"github.com/spf13/cobra"
 )
+
 var echoTimes int
 var priceCmd = &cobra.Command{
-	Use:   "hugo",
-	Short: "Hugo is a very fast static site generator",
-	Long: `A Fast and Flexible Static Site Generator built with
-                love by spf13 and friends in Go.
-                Complete documentation is available at https://gohugo.io/documentation/`,
+	Use:   "Bitcoin helper",
+	Short: "This program will contain a list of bitcoin helper commands",
+	Long: `A Fast and Flexible bitcoin helper commands built with
+                love by joseguru and friends in Go.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// Do Stuff Here
-        resp, err := http.Get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD")
+		resp, err := http.Get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD")
 		if err != nil {
 			log.Fatalln(err)
 		}
@@ -29,7 +29,7 @@ var priceCmd = &cobra.Command{
 		//Convert the body to type string
 		fmt.Println(string(body))
 		defer resp.Body.Close()
-		
+
 	},
 }
 
